6ae567b4cd98d7fd7e81c809b0668c8883469bf0
Vincent Smithers <vsmithers1087@gmail.com>
2019-06-13 19:21:27 +0200

    Make NetworkingLiteClient init public

--- Sources/NetworkingLite/NetworkingLiteClient.swift
+++ Sources/NetworkingLite/NetworkingLiteClient.swift
@@ -10,6 +10,7 @@ import Foundation
 /// Object that abstracts network requests. Initialize and store instance in location that won't be deallocated; don't initialize class in function scope
 final public class NetworkingLiteClient<WebServiceConfig: WebServiceConfiguration>: WebServiceClient {
     
+    public init() {}
     /// Makes request for type that conforms to `WebServiceConfiguration`
     /// - Parameter webServiceConfig: The associated type containing all request cases. Request case declared when method is called
     /// - Parameter result: Type `WebRequestResult` consisting of `success` and `error`
